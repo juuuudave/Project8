51ed297830bc869a5d08481304ede81a2c3090dc
juuuudave <justingendron02@gmail.com>
2023-02-23 18:30:11 +0100

    écriture des liens dans le fichiers .csv

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 # L'url du site que je souhaite Scraper
 baseUrl = 'https://www.stage.fr/'
-uri = "jobs/?q=stage%20Cybersecurité&p="
+uri = "jobs/?q=stage%20cybersécurité&p=1"
 
 
 #Génère des liens avec l'argument "page" qui s'incrémente
@@ -38,6 +38,7 @@ def swoup(url, process):
     
     else:
         print("ERROR: Failed Connect")
+        pass
     return
 
 
@@ -67,8 +68,8 @@ def addBaseUrl(baseUrl, urls):
     return res
 
 
-# Fonction qui permet de "scrapper" sur le site et récupérer tous les information sur les pages visées
 def getInformations(soup):
+    # Fonction qui permet de "scrapper" sur le site et récupérer tous les information sur les pages visées
     # pour le titre du stage
     title_stages = soup.find('h1', {"class": "details-header__title"})
     title = title_stages.getText()
@@ -77,65 +78,77 @@ def getInformations(soup):
     # on vérifie si les informations ne sont pas vide
     if title is not None:
         ul = soup.find("ul", {"class": "clearfix"}) # ex du prof
+        if ul is not None:
+            name = ul.find("li", {"class": "listing-item__info--item-company"}).getText()
+            if name is not None:
+                address = ul.find("li", {"class": "listing-item__info--item-location"}).getText()
+                if address is not None:
+                    date = ul.find("li", {"class": "listing-item__info--item-date"}).getText()
+                    
+                    # try:
+                    #     cleanAddress = []
+                    #     for ele in str(address).split("\n"):
+                    #         if ele.strip() != "":
+                    #             cleanAddress = address
 
-        name = ul.find("li", {"class": "listing-item__info--item-company"}).getText()
-        address = ul.find("li", {"class": "listing-item__info--item-location"}).getText()
-        date = ul.find("li", {"class": "listing-item__info--item-date"}).getText()
+                    # except:
+                    #     # mettre les variables vide comme ceci 'address' : ""
+                    #     fiche = {
+                    #         "title" : title.replace("mettre ce qu'il faut", ""),
+                    #         "parution_date" : date,
+                    #         "address" : address,
+                    #         "name" : name
+                    #     }
 
-        # try:
-            
-        #     cleanAddress = []
-        #     for ele in str(address).split("\n"):
-        #         if ele.strip() != "":
-        #             cleanAddress = address
-
-        # except:
-        #     # mettre les variables vide comme ceci 'address' : ""
-        #     fiche = {
-        #         "title" : title.replace("mettre ce qu'il faut", ""),
-        #         "parution_date" : date,
-        #         "address" : address,
-        #         "name" : name
-        #     }
-
-        #     fiche.append()
-        #     print(fiche)
-        #     return fiche
+                    #     fiche.append()
+                    #     print(fiche)
+                    #     return fiche
 
 # fonction pour clean les infos erroné
-# def tryToCleanOrReturnBlank:
+# def tryToCleanOrReturnBlank(str):
 
+# fonction pour écrire dans un fichier
+def fileWriter(file,fieldnames, data):
+    with open(file, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+    print("l'url à été écrite")
+    
 
 # On fait un tableau vide de toutes les urls
 urls = []
-for link in getLinks(baseUrl + uri, 1):
+for link in getLinks(baseUrl + uri, 2):
     print("Checking " + link)
+
+    # on ajoute les urls dans le tableau
     urls.extend(addBaseUrl(baseUrl, swoup(link, getEndpoints)))
-    for url_link in urls:    
-        # On affiche les liens 
-        print("Voici les liens des stages :",url_link)
-        # récupération des informations
+
+    for url_link in urls:
+
+        for i in range(len(urls)):
+            rows.append({
+                "id": i,
+                "category":" liens ",
+                "link": urls[i]
+            })
+        
+            # catégorie du fichier 
+            headers = ["id", "category", "link"]
+            
+            with open('linkListStage.csv', "w", encoding='UTF8' ,newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=headers)
+                writer.writeheader()
+                for row in rows:
+                    writer.writerow(row)
+                    
+        # On affiche les liens
+        print("Voici les liens des stages :", url_link)
+
+        # Récupération des informations
         # On vérifie si on peut se connecter à la page 
         swoup(url_link, getInformations)
-        # time.sleep()
-
-
-
-
-        # Les titres de nos informations
-        # headers = ['id', "category", "link"]
-        
-        # rows = []
-        # with open('linkList.csv', "w", newline='' ) as file: 
-        #     writer = csv.DictWriter(file, fieldnames=headers)
-        #     writer.writeheader()
-        #     for row in rows: 
-        #         writer.writerow(row)
+        rows = []
         
         
-        # for i in range(urls): 
-        #     rows.append({
-        #         "id" : i,
-        #         "category" : "None",
-        #         "link" : urls[i]
-        #     })
+
--- proxies.py
+++ proxies.py
@@ -32,13 +32,11 @@ proxy_list.head()
 https_proxies = proxy_list[proxy_list["Https"] == "yes"]
 # on les comptes 
 https_proxies.count()
-print(https_proxies.count())
-
 
 
 url = "https://httpbin.org/ip"
 good_proxies = set()
-headers = browser_headers["Firefox"]
+headers = browser_headers["Chrome"]
 for proxy_url in https_proxies["url"]:
     proxies = {
         "http": proxy_url,
@@ -49,28 +47,27 @@ for proxy_url in https_proxies["url"]:
         response = requests.get(url, headers=headers, proxies=proxies, timeout=2)
         good_proxies.add(proxy_url)
         # print(f"Proxy {proxy_url} OK, added to good_proxy list")
+
     except Exception:
         pass
-    
-    if len(good_proxies) >= 3:
+
+    if len(good_proxies) == 10:
         break
 
 
-# test avec Selenium
-# for proxy_url in good_proxies:
-#     proxy = proxy_url.replace("http://", "")
 
-#     firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
-#     firefox_capabilities['marionette'] = True
+url = "https://httpbin.org/anything"
+for browser, headers in browser_headers.items():
+    print(f"\n\nUsing {browser} headers\n")
 
-#     firefox_capabilities['proxy'] = {
-#         "proxyType": "MANUAL",
-#         "httpProxy": proxy,
-#         "sslProxy": proxy
-#     }
-
-#     driver = webdriver.Firefox(capabilities=firefox_capabilities)
-#     try:
-#         driver.get("https://httpbin.org/ip")
-#     except Exception:
-#         pass
+    for proxy_url in good_proxies:
+        proxies = proxies = {
+            "http": proxy_url,
+            "https": proxy_url,
+        }
+        try:
+            response = requests.get(url, headers=headers, proxies=proxies, timeout=2)
+            
+            print(response.json())
+        except Exception:
+            print(f"Proxy {proxy_url} failed, trying another one")
